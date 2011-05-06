e4b842c40e39bb9a26e7f5c759bba42c0ee6cdd8
Andy Sturges <arsturges@lbl.gov>
2011-05-06 16:48:04 -0700

    cut out unnecessary loop

--- monthly_averages_by_region.rb
+++ monthly_averages_by_region.rb
@@ -1,38 +1,38 @@
 require 'rubygems'
 require 'numru/netcdf'
 require 'csv'
-tas_co2_doubling = NumRu::NetCDF.open("../NOAA_CM2.1_NetCDF_files/1%_tas_A1.020101-022012.nc")
+file = NumRu::NetCDF.open("../NOAA_CM2.1_NetCDF_files/1%_tas_A1.020101-022012.nc")
 # Goal: return tas data for just North America. 
 # This means I should limit all data to lon/lat box of:
 # NW corner: Latitude 50, longitude -125 (longitude +235)
 # SE corner: Lattidue 25, longitude -67 (longitude +293)
-geographic_point_total = 0
-western_longitude_boundary = 112#95
+tas_data = Array.new
+sum_of_temperatures = 0
+western_longitude_boundary = 95
 eastern_longitude_boundary = 113
-southern_latitude_boundary = 67#60
+southern_latitude_boundary = 60
 northern_latitude_boundary = 68
 total_number_of_geographic_points = (eastern_longitude_boundary - western_longitude_boundary + 1)*(northern_latitude_boundary - southern_latitude_boundary + 1)
 tas_data = Array.new
-(0..4).each do |month|
+number_of_months = file.var("time").shape_current[0] - 1
+
+(0..number_of_months).each do |month|
   (western_longitude_boundary..eastern_longitude_boundary).each do |longitude|
     (southern_latitude_boundary..northern_latitude_boundary).each do |latitude|
-      (1..total_number_of_geographic_points).each do |geographic_point|
-        geographic_point_total = geographic_point_total + tas_co2_doubling.var("tas").get[longitude, latitude, month]
-        if geographic_point == total_number_of_geographic_points
-          tas_data[month] = geographic_point_total/total_number_of_geographic_points
-          geographic_point_total = 0
-        end
-
-        puts "longitude = #{longitude}"
-        puts "latitude = #{latitude}"
-        puts "month = #{month}"
+      sum_of_temperatures = sum_of_temperatures + file.var("tas").get[longitude, latitude, month]
+      if latitude == northern_latitude_boundary && longitude == eastern_longitude_boundary
+	tas_data[month] = sum_of_temperatures/total_number_of_geographic_points
+	sum_of_temperatures = 0
+        puts "Month=#{month} out of #{number_of_months}"
       end
     end
   end
 end
+
 puts tas_data
+
 CSV.open("tas_doubling.csv", "w") do |write_new_row|
-tas_data.each do |row|
-write_new_row << [row]
-end
+  tas_data.each do |row|
+    write_new_row << [row]
+  end
 end
