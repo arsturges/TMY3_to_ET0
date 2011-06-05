d7bbfa9961390dfef31a6ee4870453b10b6f029b
Andy Sturges <arsturges@lbl.gov>
2011-06-04 19:57:56 -0700

    removed unnecessary methods

--- penman-monteith.rb
+++ penman-monteith.rb
@@ -47,12 +47,6 @@ def compute_Rns(_Rs, albedo=0.23)
   _Rns = (1 - albedo) * _Rs
 end
 
-def compute_N(sunset_hour_angle)
-  #equation 34
-  #angle needs to be in radians
-  _N = (24/Math::PI) * sunset_hour_angle
-end
-
 def compute_solar_declination(number_of_day_in_year)
   #equation 24
   #this returns an angle in radians, oscillating between +/-0.4 rads (+/- 22 degrees)
@@ -80,7 +74,7 @@ end
 
 #hourly-specific methods below.
 
-def compute_Rnl_hourly(_T, ea, _Rs, _Rso)
+def compute_Rnl(_T, ea, _Rs, _Rso)
   # equation 39
   stefan_boltzmann=(4.903/24) * 10**(-9) #MJ/m2-hour
   t_hr_k4 = (_T + 273.16)**4
@@ -88,7 +82,7 @@ def compute_Rnl_hourly(_T, ea, _Rs, _Rso)
   #see note about calculating Rnl with hourly vs. daily time steps in chapter 4.
 end
 
-def compute_Ra_hourly(dr, solar_declination, latitude, solar_time_angle_start, solar_time_angle_end)
+def compute_Ra(dr, solar_declination, latitude, solar_time_angle_start, solar_time_angle_end)
   #equation 28
   solar_constant = 0.0820 #MJ/m2day
   _Ra = ((12*60)/Math::PI) * 
@@ -100,10 +94,6 @@ def compute_Ra_hourly(dr, solar_declination, latitude, solar_time_angle_start, s
     Math.cos(latitude) * 
     Math.cos(solar_declination) *
     (Math.sin(solar_time_angle_end) - Math.sin(solar_time_angle_start)))
-end
-
-def compute_is_sun_shining(direct_normal_irradiance)
-  direct_normal_irradiance > 120 ? 1 : 0
 end
 
 def compute_day_in_year(month, day)
@@ -145,8 +135,8 @@ def compute_Sc(day_in_year)
   _Sc = 0.1645 * Math.sin(2 * b) - 0.1255 * Math.cos(b) - 0.025 * Math.sin(b)
 end
 
-def compute_G_hourly(direct_normal_irradiance, _Rn)
-  # need to find what defines "daylight periods" and "nighttime"
+def compute_G(direct_normal_irradiance, _Rn)
+  # we define "daytime" as any period when sun is shining.
   if direct_normal_irradiance > 0
     period_of_day = "daytime"
   else
@@ -173,28 +163,24 @@ def compute_hourly_et0(state,
                         wind_speed,
                         global_horizontal_irradiance,
                         direct_normal_irradiance)
-  middle_day_of_the_month = 30.4 * month - 15
-  solar_declination = compute_solar_declination(middle_day_of_the_month)
+  solar_declination = compute_solar_declination(compute_day_in_year(month, day))
   sunset_hour_angle = compute_sunset_hour_angle(latitude, solar_declination)
-  dr = compute_dr(middle_day_of_the_month)
-  n = compute_is_sun_shining(direct_normal_irradiance)
+  dr = compute_dr(compute_day_in_year(month, day))
   _Lz = compute_Lz(hours_from_Greenwich)
   _Sc = compute_Sc(compute_day_in_year(month, day))
   solar_time_angle_midpoint = compute_solar_time_angle_midpoint(hour + 0.5, _Lz, longitude, _Sc)
   solar_time_angle_start = compute_solar_time_angle_start(solar_time_angle_midpoint)
   solar_time_angle_end = compute_solar_time_angle_end(solar_time_angle_midpoint)
-  _Ra = compute_Ra_hourly(dr, solar_declination, latitude, solar_time_angle_start, solar_time_angle_end)
+  _Ra = compute_Ra(dr, solar_declination, latitude, solar_time_angle_start, solar_time_angle_end)
   _Rso = compute_Rso(_Ra, elevation)
-  _N = compute_N(sunset_hour_angle)
-  #_Rs = compute_Rs_hourly(n, _N, _Ra)
   _Rs = global_horizontal_irradiance * (60 * 60) / (10**6) #convert W/m2 to MJ/hour/m2
   _Rns = compute_Rns(_Rs)
   _T = temp_hr
-  _Rnl = compute_Rnl_hourly(_T, compute_ea(dew_point), _Rs, _Rso)
+  _Rnl = compute_Rnl(_T, compute_ea(dew_point), _Rs, _Rso)
   atmospheric_pressure = compute_atmospheric_pressure(elevation)
 
   _Rn = compute_Rn(_Rns, _Rnl)
-  _G = compute_G_hourly(direct_normal_irradiance, _Rn)
+  _G = compute_G(direct_normal_irradiance, _Rn)
   _gamma = compute_gamma(atmospheric_pressure)
   _Delta = compute_Delta(_T)
   _u2 = wind_speed
@@ -205,20 +191,3 @@ def compute_hourly_et0(state,
   et0_denominator = _Delta + _gamma * (1 + 0.34 * _u2)
   et0 = et0_numerator / et0_denominator
 end
-
-#puts compute_hourly_et0("AL",
-                         #"none",
-                         #-6,
-                         #120.571428571429,
-                         #90.1111111111111,
-                         #32.6225714285714,
-                         #1,
-                         #15,
-                         #18,
-                         #12.4140552995392,
-                         #1.82115015360983,
-                         #3.06410330261137,
-                         #280, #W/m2
-                         #680, #W/m2
-                         #13.6566820276498,
-                         #2.30092165898618) 
