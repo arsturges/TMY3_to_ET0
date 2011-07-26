391e364f733cacd1ab50796cd9f7bfa1fea805e5
Andy Sturges <arsturges@lbl.gov>
2011-07-26 11:57:45 -0700

    full perturbations, new directory

--- condense_TMY3_files.rb
+++ condense_TMY3_files.rb
@@ -28,7 +28,7 @@ end
 def create_new_csv_file_populate_and_close(array)
   new_file_name = @header1[0]
   puts new_file_name
-  CSV.open("../tmy3_files_reduced_for_PM_equation_base_only/#{new_file_name}.csv", "wb") do |new_row|
+  CSV.open("../tmy3_files_reduced_for_PM_equation_with_perturbations/#{new_file_name}.csv", "wb") do |new_row|
     new_row << [@header1[0], @header1[1], @header1[2], @header1[3], @header1[4], @header1[5], @header1[6]]
     new_row << [
       "month", 
@@ -41,19 +41,19 @@ def create_new_csv_file_populate_and_close(array)
       @header2[34], 
       @header2[46], 
       @header2[64], 
-      "et0_base"#, 
-      #"et0_T_+5%", 
-      #"et0_T_-5%",
-      #"et0_T_+10%",
-      #"et0_T_-5%",
-      #"et0_Td_+5%",
-      #"et0_Td_-5%",
-      #"et0_Td_+10%",
-      #"et0_Td_-10%",
-      #"et0_T_and_Td_+5%",
-      #"et0_T_and_Td_-5%",
-      #"et0_T_and_Td_+10%",
-      #"et0_T_and_Td_-10%"
+      "et0_base", 
+      "et0_T_+5%", 
+      "et0_T_-5%",
+      "et0_T_+10%",
+      "et0_T_-5%",
+      "et0_Td_+5%",
+      "et0_Td_-5%",
+      "et0_Td_+10%",
+      "et0_Td_-10%",
+      "et0_T_and_Td_+5%",
+      "et0_T_and_Td_-5%",
+      "et0_T_and_Td_+10%",
+      "et0_T_and_Td_-10%"
     ]
     array.each do |row| #Loop through each row. Each row is one hour; 24 rows per day.
       read_hourly_data(row[0], row[1], row[4], row[7], row[25], row[31], row[34], row[46], row[64])
@@ -84,19 +84,19 @@ def create_new_csv_file_populate_and_close(array)
           @hourly_wind_speed,
           @hourly_global_horizontal_irradiance,
           @hourly_direct_normal_irradiance,
-          @hourly_sky_coverage)#,
-        #@et0_T_up_5,
-        #@et0_T_down_5, 
-        #@et0_T_up_10,
-        #@et0_T_down_10,
-        #@et0_Td_up_5,
-        #@et0_Td_down_5,
-        #@et0_Td_up_10,
-        #@et0_Td_down_10,
-        #@et0_T_and_Td_up_5,
-        #@et0_T_and_Td_down_5,
-        #@et0_T_and_Td_up_10,
-        #@et0_T_and_Td_down_10
+          @hourly_sky_coverage),
+        @et0_T_up_5,
+        @et0_T_down_5, 
+        @et0_T_up_10,
+        @et0_T_down_10,
+        @et0_Td_up_5,
+        @et0_Td_down_5,
+        @et0_Td_up_10,
+        @et0_Td_down_10,
+        @et0_T_and_Td_up_5,
+        @et0_T_and_Td_down_5,
+        @et0_T_and_Td_up_10,
+        @et0_T_and_Td_down_10
       ]
     end
   end
