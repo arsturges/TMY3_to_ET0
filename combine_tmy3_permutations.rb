fa41d278a734a1ceb16640b033823c8cb65fcafd
Andrew Sturges <andrew.r.sturges@gmail.com>
2011-10-06 11:16:30 -0400

    removed array logic, used incrementors instead

--- combine_tmy3_permutations.rb
+++ combine_tmy3_permutations.rb
@@ -2,7 +2,6 @@ require 'csv'
 require './TMY3.rb'
 
 filenames = Dir.glob('../tmy3_files_reduced_for_PM_with_perturbations/*')
-#filenames = Dir.glob('../tmy3_test_files/*')
 states = Hash.new
 filenames.sort.each do |filename|
   puts "reading file #{filename}"
@@ -11,6 +10,8 @@ filenames.sort.each do |filename|
   header2 = current_file.shift
   state = header1[2]
   states[state] ||= {}
+  states[state][:count] ||= 0
+  states[state][:count] += 1
   current_file.each do |station_row|
     month = station_row[0].to_i
     day = station_row[1].to_i
@@ -19,41 +20,41 @@ filenames.sort.each do |filename|
     states[state][month] ||= {}
     states[state][month][day] ||= {}
     states[state][month][day][hour] ||= {}
-    states[state][month][day][hour]["et0_base"] ||= []
-    states[state][month][day][hour]["et0_T_+5%"] ||= []
-    states[state][month][day][hour]["et0_T_-5%"] ||= []
-    states[state][month][day][hour]["et0_T_+10%"] ||= []
-    states[state][month][day][hour]["et0_T_-10%"] ||= []
-    states[state][month][day][hour]["et0_Td_+5%"] ||= []
-    states[state][month][day][hour]["et0_Td_-5%"] ||= []
-    states[state][month][day][hour]["et0_Td_+10%"] ||= []
-    states[state][month][day][hour]["et0_Td_-10%"] ||= []
-    states[state][month][day][hour]["et0_T_and_Td_+5%"] ||= []
-    states[state][month][day][hour]["et0_T_and_Td_-5%"] ||= []
-    states[state][month][day][hour]["et0_T_and_Td_+10%"] ||= []
-    states[state][month][day][hour]["et0_T_and_Td_-10%"] ||= []
-    states[state][month][day][hour]["et0_T_+5%_and_Td_-5%"] ||= []
-    states[state][month][day][hour]["et0_T_+10%_and_Td_-10%"] ||= []
-    states[state][month][day][hour]["et0_T_-5%_and_Td_+5%"] ||= []
-    states[state][month][day][hour]["et0_T_-10%_and_Td_+10%"] ||= []
+    states[state][month][day][hour]["et0_base"] ||= 0
+    states[state][month][day][hour]["et0_T_+5%"] ||= 0
+    states[state][month][day][hour]["et0_T_-5%"] ||= 0
+    states[state][month][day][hour]["et0_T_+10%"] ||= 0
+    states[state][month][day][hour]["et0_T_-10%"] ||=0
+    states[state][month][day][hour]["et0_Td_+5%"] ||= 0
+    states[state][month][day][hour]["et0_Td_-5%"] ||= 0
+    states[state][month][day][hour]["et0_Td_+10%"] ||= 0
+    states[state][month][day][hour]["et0_Td_-10%"] ||= 0
+    states[state][month][day][hour]["et0_T_and_Td_+5%"] ||= 0
+    states[state][month][day][hour]["et0_T_and_Td_-5%"] ||= 0
+    states[state][month][day][hour]["et0_T_and_Td_+10%"] ||= 0
+    states[state][month][day][hour]["et0_T_and_Td_-10%"] ||= 0
+    states[state][month][day][hour]["et0_T_+5%_and_Td_-5%"] ||= 0
+    states[state][month][day][hour]["et0_T_+10%_and_Td_-10%"] ||= 0
+    states[state][month][day][hour]["et0_T_-5%_and_Td_+5%"] ||= 0
+    states[state][month][day][hour]["et0_T_-10%_and_Td_+10%"] ||= 0
 
-    states[state][month][day][hour]["et0_base"] << sprintf("%.3f", station_row[10].to_f)
-    states[state][month][day][hour]["et0_T_+5%"] << sprintf("%.3f", station_row[11].to_f)
-    states[state][month][day][hour]["et0_T_-5%"] << sprintf("%.3f", station_row[12].to_f)
-    states[state][month][day][hour]["et0_T_+10%"] << sprintf("%.3f", station_row[13].to_f)
-    states[state][month][day][hour]["et0_T_-10%"] << sprintf("%.3f", station_row[14].to_f)
-    states[state][month][day][hour]["et0_Td_+5%"] << sprintf("%.3f", station_row[15].to_f)
-    states[state][month][day][hour]["et0_Td_-5%"] << sprintf("%.3f", station_row[16].to_f)
-    states[state][month][day][hour]["et0_Td_+10%"] << sprintf("%.3f", station_row[17].to_f)
-    states[state][month][day][hour]["et0_Td_-10%"] << sprintf("%.3f", station_row[18].to_f)
-    states[state][month][day][hour]["et0_T_and_Td_+5%"] << sprintf("%.3f", station_row[19].to_f)
-    states[state][month][day][hour]["et0_T_and_Td_-5%"] << sprintf("%.3f", station_row[20].to_f)
-    states[state][month][day][hour]["et0_T_and_Td_+10%"] << sprintf("%.3f", station_row[21].to_f)
-    states[state][month][day][hour]["et0_T_and_Td_-10%"] << sprintf("%.3f", station_row[22].to_f)
-    states[state][month][day][hour]["et0_T_+5%_and_Td_-5%"] << sprintf("%.3f", station_row[23].to_f)
-    states[state][month][day][hour]["et0_T_+10%_and_Td_-10%"] << sprintf("%.3f", station_row[24].to_f)
-    states[state][month][day][hour]["et0_T_-5%_and_Td_+5%"] << sprintf("%.3f", station_row[25].to_f)
-    states[state][month][day][hour]["et0_T_-10%_and_Td_+10%"] << sprintf("%.3f", station_row[26].to_f)
+    states[state][month][day][hour]["et0_base"] += station_row[10].to_f
+    states[state][month][day][hour]["et0_T_+5%"] += station_row[11].to_f
+    states[state][month][day][hour]["et0_T_-5%"] += station_row[12].to_f
+    states[state][month][day][hour]["et0_T_+10%"] += station_row[13].to_f
+    states[state][month][day][hour]["et0_T_-10%"] += station_row[14].to_f
+    states[state][month][day][hour]["et0_Td_+5%"] += station_row[15].to_f
+    states[state][month][day][hour]["et0_Td_-5%"] += station_row[16].to_f
+    states[state][month][day][hour]["et0_Td_+10%"] += station_row[17].to_f
+    states[state][month][day][hour]["et0_Td_-10%"] += station_row[18].to_f
+    states[state][month][day][hour]["et0_T_and_Td_+5%"] += station_row[19].to_f
+    states[state][month][day][hour]["et0_T_and_Td_-5%"] += station_row[20].to_f
+    states[state][month][day][hour]["et0_T_and_Td_+10%"] += station_row[21].to_f
+    states[state][month][day][hour]["et0_T_and_Td_-10%"] += station_row[22].to_f
+    states[state][month][day][hour]["et0_T_+5%_and_Td_-5%"] += station_row[23].to_f
+    states[state][month][day][hour]["et0_T_+10%_and_Td_-10%"] += station_row[24].to_f
+    states[state][month][day][hour]["et0_T_-5%_and_Td_+5%"] += station_row[25].to_f
+    states[state][month][day][hour]["et0_T_-10%_and_Td_+10%"] += station_row[26].to_f
   end
 end
 
@@ -89,23 +90,23 @@ states.keys.sort.each do |state|
             month,
             day,
             hour,
-            sprintf("%.3f", sum(states[state][month][day][hour]["et0_base"])/states[state][month][day][hour]["et0_base"].size),
-            sprintf("%.3f", sum(states[state][month][day][hour]["et0_T_+5%"])/states[state][month][day][hour]["et0_T_+5%"].size),
-            sprintf("%.3f", sum(states[state][month][day][hour]["et0_T_-5%"])/states[state][month][day][hour]["et0_T_-5%"].size),
-            sprintf("%.3f", sum(states[state][month][day][hour]["et0_T_+10%"])/states[state][month][day][hour]["et0_T_+10%"].size),
-            sprintf("%.3f", sum(states[state][month][day][hour]["et0_T_-10%"])/states[state][month][day][hour]["et0_T_-10%"].size),
-            sprintf("%.3f", sum(states[state][month][day][hour]["et0_Td_+5%"])/states[state][month][day][hour]["et0_Td_+5%"].size),
-            sprintf("%.3f", sum(states[state][month][day][hour]["et0_Td_-5%"])/states[state][month][day][hour]["et0_Td_-5%"].size),
-            sprintf("%.3f", sum(states[state][month][day][hour]["et0_Td_+10%"])/states[state][month][day][hour]["et0_Td_+10%"].size),
-            sprintf("%.3f", sum(states[state][month][day][hour]["et0_Td_-10%"])/states[state][month][day][hour]["et0_Td_-10%"].size),
-            sprintf("%.3f", sum(states[state][month][day][hour]["et0_T_and_Td_+5%"])/states[state][month][day][hour]["et0_T_and_Td_+5%"].size),
-            sprintf("%.3f", sum(states[state][month][day][hour]["et0_T_and_Td_-5%"])/states[state][month][day][hour]["et0_T_and_Td_-5%"].size),
-            sprintf("%.3f", sum(states[state][month][day][hour]["et0_T_and_Td_+10%"])/states[state][month][day][hour]["et0_T_and_Td_+10%"].size),
-            sprintf("%.3f", sum(states[state][month][day][hour]["et0_T_and_Td_-10%"])/states[state][month][day][hour]["et0_T_and_Td_-10%"].size)
-            sprintf("%.3f", sum(states[state][month][day][hour]["et0_T_+5%_and_Td_-5%"])/states[state][month][day][hour]["et0_T_+5%_and_Td_-5%"].size),
-            sprintf("%.3f", sum(states[state][month][day][hour]["et0_T_+10%_and_Td_-10%"])/states[state][month][day][hour]["et0_T_+10%_and_Td_-10%"].size),
-            sprintf("%.3f", sum(states[state][month][day][hour]["et0_T_-5%_and_Td_+5%"])/states[state][month][day][hour]["et0_T_-5%_and_Td_+5%"].size),
-            sprintf("%.3f", sum(states[state][month][day][hour]["et0_T_-10%_and_Td_+10%"])/states[state][month][day][hour]["et0_T_-10%_and_Td_+10%"].size)
+            sprintf("%.2f", states[state][month][day][hour]["et0_base"]/states[state][:count]),
+            sprintf("%.2f", states[state][month][day][hour]["et0_T_+5%"]/states[state][:count]),
+            sprintf("%.2f", states[state][month][day][hour]["et0_T_-5%"]/states[state][:count]),
+            sprintf("%.2f", states[state][month][day][hour]["et0_T_+10%"]/states[state][:count]),
+            sprintf("%.2f", states[state][month][day][hour]["et0_T_-10%"]/states[state][:count]),
+            sprintf("%.2f", states[state][month][day][hour]["et0_Td_+5%"]/states[state][:count]),
+            sprintf("%.2f", states[state][month][day][hour]["et0_Td_-5%"]/states[state][:count]),
+            sprintf("%.2f", states[state][month][day][hour]["et0_Td_+10%"]/states[state][:count]),
+            sprintf("%.2f", states[state][month][day][hour]["et0_Td_-10%"]/states[state][:count]),
+            sprintf("%.2f", states[state][month][day][hour]["et0_T_and_Td_+5%"]/states[state][:count]),
+            sprintf("%.2f", states[state][month][day][hour]["et0_T_and_Td_-5%"]/states[state][:count]),
+            sprintf("%.2f", states[state][month][day][hour]["et0_T_and_Td_+10%"]/states[state][:count]),
+            sprintf("%.2f", states[state][month][day][hour]["et0_T_and_Td_-10%"]/states[state][:count]),
+            sprintf("%.2f", states[state][month][day][hour]["et0_T_+5%_and_Td_-5%"]/states[state][:count]),
+            sprintf("%.2f", states[state][month][day][hour]["et0_T_+10%_and_Td_-10%"]/states[state][:count]),
+            sprintf("%.2f", states[state][month][day][hour]["et0_T_-5%_and_Td_+5%"]/states[state][:count]),
+            sprintf("%.2f", states[state][month][day][hour]["et0_T_-10%_and_Td_+10%"]/states[state][:count])
           ]
         end
       end
